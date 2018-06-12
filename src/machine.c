97c0a79e169412d701a476f23aa7ee8e9d06687d
soufiane jounaid <soufianejounaid@gmail.com>
2018-06-12 17:18:36 +0200

    Parsed using FREAD Implemented task 1, passes all tests

--- src/machine.c
+++ src/machine.c
@@ -1,109 +1,171 @@
 #include <ijvm.h>
+#include <stdlib.h>
+#include <assert.h>
+
 
 int counter = 0;
+
+
+byte_t *text_data;
+word_t *const_data;
+
+word_t size_text;
+
+
+
+static word_t swap_uint32(word_t num)
+{
+  return ((num>>24)&0xff) | ((num<<8)&0xff0000) |
+  ((num>>8)&0xff00) |  ((num<<24)&0xff000000);
+}
+
+word_t read_word(FILE *fp)
+{
+  word_t value;
+
+  fread(&value, sizeof(word_t), 1, fp);
+  return swap_uint32(value);
+}
+
 
 int init_ijvm(char *binary_file)
 {
   FILE *fp;
 
   fp = fopen(binary_file, "rb");
+  assert(fp != NULL);
 
-  int size_array = 0;
-  int indic = 0; // variable that keeps track of const pool data size + current array pointer
+  word_t magic_num    = read_word(fp);
+  assert(magic_num == MAGIC_NUMBER);
 
-  fseek(fp, 0L, SEEK_END);
-  size_array = ftell(fp);
-  rewind(fp);
+  word_t const_origin = read_word(fp);
+  word_t const_size   = read_word(fp);
+  const_data = malloc(const_size);
 
-  byte_t array[size_array];
-  byte_t byte;
-
-  word_t const_size;
-  word_t magic_num;
-  word_t text_size;
-
-
-
-  while (true)
+  for (word_t i = 0; i < const_size / 4; i++)
   {
-    byte = fgetc(fp);
-
-    if (feof(fp)) {break;}
-
-    counter += 1;
-    array[counter] = byte;
-
-    if (counter == 4)
-    {
-      magic_num = (array[counter - 3] << 24) + (array[counter - 2] << 16) + (array[counter - 1] << 8) + array[counter];
-    }
-
-    if (counter == 12)
-    {
-      const_size = (array[counter - 3] << 24) + (array[counter - 2] << 16) + (array[counter - 1] << 8) + array[counter];
-      indic += const_size;
-      indic += 20; // indicates next relevant pointer = magic_num + const origin + const pool size + text origin + text size
-    }
-
-    if (counter == indic)
-    {
-      text_size = (array[counter - 3] << 24) + (array[counter - 2] << 16) + (array[counter - 1] << 8) + array[counter];
-    }
-
-  } // while end
-
-  for (int i = 1; i < size_array + 1; i++)
-  {
-    printf("%x", array[i]);
+    const_data[i] = (word_t) read_word(fp);
   }
 
-  printf("\n%x\n", magic_num);
-  printf("%x\n", const_size);
-  printf("%x\n", text_size);
+  word_t text_origin = read_word(fp);
+  size_text   = read_word(fp);
+  text_data = malloc(size_text);
 
-  for (int opcode = indic ; opcode <= indic + text_size; opcode++)
-  {
-    switch (array[opcode])
-    {
-      case OP_BIPUSH:
-        printf("%s\n", "BIPUSH");
-        opcode += 1;
-        break;
+  fread(text_data, sizeof(byte_t), size_text, fp);
 
-      case OP_IADD:
-        printf("%s\n", "IADD");
-        break;
+  fclose(fp);
 
-      case OP_OUT:
-        printf("%s\n", "OUT");
-        break;
-
-      case OP_HALT:
-        printf("%s\n", "HALT");
-        break;
-    }
-    counter += 1;
-  }
 
   return 0;
 }
+
+char* match(int counter)
+{
+  char *opcode;
+    switch (text_data[counter])
+    {
+      case OP_BIPUSH:
+        opcode = "BIPUSH";
+        counter++;
+        break;
+      case OP_DUP:
+        opcode = "DUP";
+        break;
+      case OP_ERR:
+        opcode = "ERR";
+        break;
+      case OP_GOTO:
+        opcode = "GOTO";
+        break;
+      case OP_HALT:
+        opcode = "";
+        break;
+      case OP_IADD:
+        opcode = "IADD";
+        break;
+      case OP_IAND:
+        opcode = "IAND";
+        break;
+      case OP_IFEQ:
+        opcode = "IFEQ";
+        break;
+      case OP_IFLT:
+        opcode = "IFLT";
+        break;
+      case OP_ICMPEQ:
+        opcode = "ICMPEQ";
+        break;
+      case OP_IINC:
+        opcode = "IINC";
+        break;
+      case OP_ILOAD:
+        opcode = "ILOAD";
+        break;
+      case OP_IN:
+        opcode = "IN";
+        break;
+      case OP_INVOKEVIRTUAL:
+        opcode = "INVOKEVIRTUAL";
+        break;
+      case OP_IOR:
+        opcode = "IOR";
+        break;
+      case OP_IRETURN:
+        opcode = "IRETURN";
+        break;
+      case OP_ISTORE:
+        opcode = "ISTORE";
+        break;
+      case OP_ISUB:
+        opcode = "ISUB";
+        break;
+      case OP_LDC_W:
+        opcode = "LDC_W";
+        break;
+      case OP_NOP:
+        opcode = "NOP";
+        break;
+      case OP_OUT:
+        opcode = "OUT";
+        break;
+      case OP_POP:
+        opcode = "POP";
+        break;
+      case OP_SWAP:
+        opcode = "SWAP";
+        break;
+      case OP_WIDE:
+        opcode = "WIDE";
+        break;
+      default:
+        opcode="";
+        break;
+    }
+      return opcode;
+}
+
+
+
+
 
 bool step(void)
 {
+  printf("%s\n", match(counter));
+  counter += 1;
   return true;
 }
 
-int text_size(void)
+int text_size(void) // done
 {
-  return 0;
+  return size_text;
 }
 
-byte_t *get_text(void)
+byte_t *get_text(void) // TODO
 {
-  return 0;
+  return text_data;
 }
 
-int get_program_counter(void)
+int get_program_counter(void) // done
 {
   return counter;
 }
@@ -115,7 +177,9 @@ byte_t get_instruction(void)
 
 void destroy_ijvm()
 {
-  // Reset IJVM state
+  counter = 0;
+  free(text_data);
+  free(const_data);
 }
 
 void run()
